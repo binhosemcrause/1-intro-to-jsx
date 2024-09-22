3cf5aecff7333685edf92eae7447b0f441721b4c
binhosemcrause <binhosemcrause@gmail.com>
2024-09-22 15:15:07 +1000

    Project example to work with JSX

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Import React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//You first create  the root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Create HTML element
+const element = <h1>Hello</h1>
+
+//Render the HTML element on the root
+root.render(element);
